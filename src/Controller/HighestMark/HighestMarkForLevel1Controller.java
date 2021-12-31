138e8dd7aecc43dc7581e8199c24b80425da9a9f
Mohamed Shelf <mohamedsaidlearn@gmail.com>
2022-01-01 01:04:48 +0200

    'Highest Mark For Level 1' Page Design & Controller variables injection

---
+++ src/Controller/HighestMark/HighestMarkForLevel1Controller.java
@@ -0,0 +1,28 @@
+package Controller.HighestMark;
+
+
+import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+
+public class HighestMarkForLevel1Controller {
+    
+    @FXML
+    private ImageView BackBtn;
+    
+    @FXML
+    private JFXButton GPA;
+    
+    @FXML
+    private JFXButton Home;
+    
+    @FXML
+    private JFXButton MaterialRegistration;
+    
+    @FXML
+    private JFXButton SOTY;
+    
+    @FXML
+    private JFXButton ShowMarks;
+    
+}
