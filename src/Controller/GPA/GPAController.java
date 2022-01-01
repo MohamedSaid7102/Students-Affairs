dda3dd3f46adf79c44e5fc46f3f526b76adeee7c
Mohamed Shelf <mohamedsaidlearn@gmail.com>
2022-01-01 03:01:06 +0200

    1. 'GPA for Level 1 students' Page Design 2. added all GPA scenes in one package

---
+++ src/Controller/GPA/GPAController.java
@@ -0,0 +1,32 @@
+package Controller.GPA;
+
+
+import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class GPAController {
+    
+    @FXML
+    private ImageView BackBtn;
+    
+    @FXML
+    private JFXButton GPA;
+    
+    @FXML
+    private JFXButton Home;
+    
+    @FXML
+    private JFXButton MaterialRegistration;
+    
+    @FXML
+    private JFXButton SOTY;
+    
+    @FXML
+    private JFXButton ShowMarks;
+    
+    @FXML
+    private Label StudentGPA;
+    
+}
---
+++ src/Controller/GPA/GPAForLevel1View.java
@@ -0,0 +1,27 @@
+package Controller.GPA;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+
+public class GPAForLevel1View {
+    
+    @FXML
+    private ImageView BackBtn;
+    
+    @FXML
+    private JFXButton GPA;
+    
+    @FXML
+    private JFXButton Home;
+    
+    @FXML
+    private JFXButton MaterialRegistration;
+    
+    @FXML
+    private JFXButton SOTY;
+    
+    @FXML
+    private JFXButton ShowMarks;
+    
+}
